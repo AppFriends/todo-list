42f04091d66b9d80edd232ab24cde9396d4e854b
Rafael França <euconectei@MacBook-Pro-de-Rafael.local>
2016-09-29 22:33:25 -0300

    Setup inicial do gulp e atualização do gitignore

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./src"
+        },
+        port: 7001
+    });
+});
+
+gulp.task('default', ['browserSync']);
